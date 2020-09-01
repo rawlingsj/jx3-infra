7a156d0a0498d5e1ac415f70c2922be38ccb9743
James Rawlings <rawlingsj80@gmail.com>
2020-09-01 15:28:37 +0100

    chore: fix terraform plan and use unique resource names

--- modules/bdd/serviceaccounts.tf
+++ modules/bdd/serviceaccounts.tf
@@ -1,32 +1,32 @@
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
+resource "google_project_iam_member" "tekton_sa_owner_binding" {
   provider = google
   role     = "roles/owner"
   member   = "serviceAccount:${var.tekton_sa_email}"
   project = "jenkins-x-labs-bdd"
 }
 
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
+resource "google_project_iam_member" "tekton_sa_iam_sec_admin_binding" {
   provider = google
   role     = "roles/iam.securityAdmin"
   member   = "serviceAccount:${var.tekton_sa_email}"
   project = "jenkins-x-labs-bdd"
 }
 
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
+resource "google_project_iam_member" "tekton_sa_iam_prj_admin_binding" {
   provider = google
   role     = "roles/resourcemanager.projectIamAdmin"
   member   = "serviceAccount:${var.tekton_sa_email}"
   project = "jenkins-x-labs-bdd"
 }
 
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
+resource "google_project_iam_member" "tekton_sa_access_policy_admin_binding" {
   provider = google
   role     = "roles/accesscontextmanager.policyAdmin"
   member   = "serviceAccount:${var.tekton_sa_email}"
   project = "jenkins-x-labs-bdd"
 }
 
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
+resource "google_project_iam_member" "tekton_sa_resource_prj_get_binding" {
   provider = google
   role     = "roles/resourcemanager.projects.getIamPolicy"
   member   = "serviceAccount:${var.tekton_sa_email}"
