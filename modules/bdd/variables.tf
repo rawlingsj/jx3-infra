a44ef19feb7af4f6cd1eb7ab019fa7374fd1b72d
James Rawlings <rawlingsj80@gmail.com>
2020-09-23 22:17:43 +0100

    fix: vars

--- modules/bdd/variables.tf
+++ modules/bdd/variables.tf
@@ -5,3 +5,11 @@ variable "tekton_sa_email" {
   description = "The name of the GCP project to use"
   type        = string
 }
+// ----------------------------------------------------------------------------
+// Optional Variables
+// ----------------------------------------------------------------------------
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster to create"
+  type        = string
+  default     = ""
+}
