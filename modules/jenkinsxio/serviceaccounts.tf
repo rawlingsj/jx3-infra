2c5910446fd1f540e61350a2dc736d4761a00258
James Rawlings <rawlingsj80@gmail.com>
2021-06-30 10:56:24 +0100

    chore: remove admin karma as we now publish to github

--- modules/jenkinsxio/serviceaccounts.tf
+++ modules/jenkinsxio/serviceaccounts.tf
@@ -1,30 +1,16 @@
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
-  provider = google
-  role     = "roles/storage.objectAdmin"
-  member   = "serviceAccount:${var.tekton_sa_email}"
-  project = "jenkinsxio"
-}
+# resource "google_project_iam_member" "tekton_sa_cloud_builder_binding" {
+#   provider = google
+#   role     = "roles/cloudbuild.builds.builder"
+#   member   = "serviceAccount:${var.tekton_sa_email}"
+#   project = "jenkinsxio"
+# }
 
-resource "google_project_iam_member" "tekton_sa_storage_admin_binding" {
-  provider = google
-  role     = "roles/storage.admin"
-  member   = "serviceAccount:${var.tekton_sa_email}"
-  project = "jenkinsxio"
-}
-
-resource "google_project_iam_member" "tekton_sa_cloud_builder_binding" {
-  provider = google
-  role     = "roles/cloudbuild.builds.builder"
-  member   = "serviceAccount:${var.tekton_sa_email}"
-  project = "jenkinsxio"
-}
-
-resource "google_project_iam_member" "tekton_sa_cloud_builder_viewlogs_binding" {
-  provider = google
-  role     = "roles/viewer"
-  member   = "serviceAccount:${var.tekton_sa_email}"
-  project = "jenkinsxio"
-}
+# resource "google_project_iam_member" "tekton_sa_cloud_builder_viewlogs_binding" {
+#   provider = google
+#   role     = "roles/viewer"
+#   member   = "serviceAccount:${var.tekton_sa_email}"
+#   project = "jenkinsxio"
+# }
 
 // ----------------------------------------------------------------------------
 // BDD bot
